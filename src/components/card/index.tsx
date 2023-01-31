6e7be3b5fe5c4403f8f2a6ee9ffb2f9728054d09
Guilherme Vargas <guisouzavargas@outlook.com>
2023-01-31 16:31:21 -0300

    NextTravelsCard development

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,8 +2,8 @@ import { iCard } from "../../types/CardType"
 
 export default function Card({ title, alt, description, image }: iCard) {
     return (
-        <div className="h-auto text-center">
-            <img className="w-16 ml-auto mr-auto left-0 right-0" src={image} alt="" />
+        <div className="flex flex-col items-center">
+            <img className="w-full h-60 ml-auto mr-auto left-0 right-0" src={image} alt="" />
             <h3 className="mt-3">{title}</h3>
             <p>{description}</p>
         </div>
--- src/components/card/nextTravelsCard/index.tsx
+++ src/components/card/nextTravelsCard/index.tsx
@@ -2,19 +2,19 @@ import Card from "..";
 
 export default function NextTravelsCard() {
     return (
-        <section className="font-padrao bg-paleta-azul3 flex flex-col items-center">
-            <div className=" text-white text-center items-center text-lg flex flex-col">
-                <h2 className=" mt-5 mb-4 text-4xl text-my-color3 font-bold">Próximos destinos</h2>
+        <section className="">
+            <div className="text-center text-3x mt-10 mb-10">
+                <h2 className="">Próximos destinos</h2>
             </div>
-            <ul className="p-10 h-auto w-96 flex flex-col gap-4 m-1 md:grid md:grid-cols-2 md:w-auto lg:grid-cols-3">
-                <li className="shadow-lg rounded-md w-auto h-60 mt-5 relative md:h-auto md:w-48 border hover:hover:border-my-color3">
-                    <Card title="Morretes" image="" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+            <ul className="flex flex-col items-center">
+                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
+                    <Card title="Morretes" image="\img\morretes.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
                 </li>
-                <li className="shadow-lg rounded-md w-auto h-60 mt-5 relative md:h-auto md:w-48 border hover:hover:border-my-color3">
-                    <Card title="Cascaneia" image="" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
+                    <Card title="Cascaneia" image="\img\cascaneia.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
                 </li>
-                <li className="shadow-lg rounded-md w-auto h-60 mt-5 relative md:h-auto md:w-48 border hover:hover:border-my-color3">
-                    <Card title="Foz do Iguaçu" image="" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
+                    <Card title="Foz do Iguaçu" image="\img\cataratas.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
                 </li>
             </ul>
         </section>
