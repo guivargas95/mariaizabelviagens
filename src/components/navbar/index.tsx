203f2c3a4dcfc2d263efaf61af899e832301bae3
Guilherme Vargas <guisouzavargas@outlook.com>
2023-01-31 09:42:45 -0300

    Logo fix

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,7 +2,7 @@ export default function Navbar(){
     return(
         
         <div className="h-25 w-auto lg:flex lg:justify-between bg-blue-400 text-white text-center">
-            <img className="ml-auto mr-auto w-28 lg:ml-36" src="\img\mariaizabel_logo.png" alt="" />
+            <img className="ml-auto mr-auto w-40 lg:ml-36" src="\img\mariaizabel_logo.png" alt="" />
             <nav className="">
                 <ul className="flex justify-around lg:mt-10 lg:mr-36">
                     <li className=" hover:text-my-color4 lg:ml-10"><a href="">Início</a></li>
