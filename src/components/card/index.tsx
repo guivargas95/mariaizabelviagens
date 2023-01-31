a5aaf8240ed909c6e881bc9d58c8116abf6f06f7
Guilherme Vargas <guisouzavargas@outlook.com>
2023-01-31 19:11:28 -0300

    Development of card and responsive design

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,11 +1,11 @@
 import { iCard } from "../../types/CardType"
 
-export default function Card({ title, alt, description, image }: iCard) {
+export default function Card({ title, alt, resume, image }: iCard) {
     return (
         <div className="flex flex-col items-center">
-            <img className="w-full h-60 ml-auto mr-auto left-0 right-0" src={image} alt="" />
-            <h3 className="mt-3">{title}</h3>
-            <p>{description}</p>
+            <img className="rounded w-full h-60 ml-auto mr-auto left-0 right-0" src={image} alt="" />
+            <h3 className="font-bold mt-3 text-2xl">{title}</h3>
+            <p className="text-center mt-6 mb-6 font-font1">{resume}</p>
         </div>
     )
 }
--- src/components/card/nextTravelsCard/index.tsx
+++ src/components/card/nextTravelsCard/index.tsx
@@ -3,18 +3,18 @@ import Card from "..";
 export default function NextTravelsCard() {
     return (
         <section className="">
-            <div className="text-center text-3x mt-10 mb-10">
-                <h2 className="">Próximos destinos</h2>
+            <div className="text-center text-3xl mt-10 mb-10 font-bold">
+                <h2 className="font-font2">Próximos destinos</h2>
             </div>
-            <ul className="flex flex-col items-center">
-                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
-                    <Card title="Morretes" image="\img\morretes.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+            <ul className="flex flex-col items-center text-amber-100 lg:grid lg:grid-cols-3">
+                <li className="bg-my-color3 h-96 w-3/4 mt-5 mb-5 shadow-lg lg:ml-auto lg:mr-auto  rounded-md hover:scale-105 hover:ease-in duration-300">
+                    <a href=""><Card title="Morretes - 10/10/2023" image="\img\morretes.jpg" alt="Foto Morretes" resume="Você sabia que um dos passeios de trem mais espetaculares do mundo, fica aqui no Paraná? Venha viver essa experiência conosco!" /></a>
                 </li>
-                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
-                    <Card title="Cascaneia" image="\img\cascaneia.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+                <li className="bg-my-color3 h-96 w-3/4 mt-5 mb-5 shadow-lg lg:ml-auto lg:mr-auto rounded-md hover:scale-105 hover:ease-in duration-300">
+                    <a href=""><Card title="Cascaneia - 10/10/2023" image="\img\cascaneia.jpg" alt="Foto Cascaneia" resume="A Cascanéia é reconhecida por ser o melhor parque aquático do sul do Brasil e o 5º melhor da América Latina. Que tal embarcar nesta aventura e curtir tudo o que só o melhor parque aquático do sul tem para oferecer?" /></a>
                 </li>
-                <li className="h-96 w-3/4 mt-5 mb-5 shadow-lg rounded-md border hover:hover:border-my-color3">
-                    <Card title="Foz do Iguaçu" image="\img\cataratas.jpg" alt="Foto Cataratas do Iguaçu" description="lorem opsudjk piosdoiaj awoidçlakslf kçawlk dçalksdowkd çlkas~dkao dko wkapdokwa kp" />
+                <li className="bg-my-color3 h-96 w-3/4 mt-5 mb-5 shadow-lg lg:ml-auto lg:mr-auto  rounded-md hover:scale-105 hover:ease-in duration-300">
+                    <a href=""><Card title="Foz do Iguaçu - 10/10/2023" image="\img\cataratas.jpg" alt="Foto Cataratas do Iguaçu" resume="Visite a Maravilha Mundial da Natureza e viva as mais raras experiências de contato com a natureza, no maior conjunto de quedas d’água do mundo, na cidade de Foz do Iguaçu." /></a>
                 </li>
             </ul>
         </section>
