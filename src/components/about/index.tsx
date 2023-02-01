f4a92fbed86282981f922c89ea3da2dd1b459bc9
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-01 20:27:50 -0300

    Created About component

---
+++ src/components/about/index.tsx
@@ -0,0 +1,10 @@
+export default function About(){
+    return(
+        <div className="flex flex-col ">
+            <ul className="p-10 lg:flex items-center text-center border lg:w-auto">
+                <li><img className="w-96 mr-auto ml-auto lg:w-auto rounded-2xl shadow-md shadow-my-color2" src="\img\infophoto.jpg" alt="" /></li>
+                <li><p className="mt-2 mb-2">Lorem ipsum aiuwdhuiawdhuiawd dawiouhdwauiw dwauihd awi dawuihd aiuwawd ui huiadwiuadhw ahuuidawui pawdijioadwjiojawdo dawji pdiawjipodaw ad wpojaw dpjip adwpijdaw pijadw ipojwapi dawjip jawipja wpijdaw pij</p></li>
+            </ul>             
+        </div>
+    )
+}
