281a8e5af7dae02c670a6fa6577821aa1f10ab28
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-03 11:13:15 -0300

    Responsive corrections

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -3,7 +3,7 @@ export default function About(){
         <div className="flex flex-col bg-blue-100cl">
             <ul className="p-10 lg:flex items-center text-center border lg:w-auto">
                 <li><img className="w-96 mr-auto ml-auto lg:w-auto rounded-2xl shadow-md shadow-my-color2" src="\img\infophoto.jpg" alt="" /></li>
-                <li className=""><p className="mt-2 mb-2 font-font1 text-my-color4 font-bold">Lorem ipsum aiuwdhuiawdhuiawd dawiouhdwauiw dwauihd awi dawuihd aiuwawd ui huiadwiuadhw ahuuidawui pawdijioadwjiojawdo dawji pdiawjipodaw ad wpojaw dpjip adwpijdaw pijadw ipojwapi dawjip jawipja wpijdaw pij</p></li>
+                <li className="flex justify-center lg:w-5/6"><p className="w-96 lg:w-5/6 mt-2 mb-2 font-font1 text-my-color4 font-bold">Olá viajante, tudo bem? Me chamo Maria Izabel e a 10 anos sou coordenadora de grupos de viagens. Meus principais roteiros são para Tríplice Fronteira, Morretes e Cascaneia. Se você quiser embarcar em uma aventura comigo, verifique abaixo as informações e passeios disponíveis, será um prazer ter você em meu grupo!</p></li>
             </ul>             
         </div>
     )
