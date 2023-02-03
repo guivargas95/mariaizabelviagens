71c43cad209c2c5e1c2cb929bd4489512590b198
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-03 11:12:54 -0300

    Test banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,12 @@ import About from "../../components/about";
 import NextTravelsCard from "../../components/card/nextTravelsCard";
 import Navbar from "../../components/navbar";
 
-export default function Index(){
-    return(
+export default function Index() {
+    return (
         <div>
-            <Navbar/>
-            <About/>
-            <NextTravelsCard/>
+            <Navbar />
+            <About />
+            <NextTravelsCard />
         </div>
     )
 }
