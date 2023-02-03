2440420aefc1181ae0fcf3d592d7b25ee4d5c088
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-03 11:11:21 -0300

    Included Gallery in navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,7 @@
+import {Link} from "react-router-dom"
+import GalleryPage from "../../pages/Gallery"
+
+
 export default function Navbar(){
     return(
         
@@ -5,9 +9,10 @@ export default function Navbar(){
             <img className="ml-auto mr-auto w-72 lg:ml-36" src="\img\mariaizabel_logo.png" alt="" />
             <nav className="">
                 <ul className="flex justify-around p-10 lg:mt-20 cl lg:mr-36 text-xl font-font1">
-                    <li className=" hover:text-my-color4 hover:font-bold lg:ml-10"><a href="">Início</a></li>
+                    <li className=" hover:text-my-color4 hover:font-bold lg:ml-10"><Link to="/">Início</Link> </li>
                     <li className=" hover:text-my-color4 hover:font-bold lg:ml-10"><a href="">Destinos</a></li>            
                     <li className=" hover:text-my-color4 hover:font-bold lg:ml-10"><a href="">Contato</a></li>
+                    <li className=" hover:text-my-color4 hover:font-bold lg:ml-10"><Link to="/galeria">Galeria</Link></li>
                 </ul>
             </nav>
         </div>
