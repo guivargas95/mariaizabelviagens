f7bcacf4e6008ac6a4ebd38d7a954e7a910b34d1
Guilherme Vargas <guisouzavargas@outlook.com>
2023-02-03 11:09:13 -0300

    Included gallery page

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,14 @@ import {
   Route
 } from "react-router-dom"
 import './reset.css'
+import GalleryPage from './pages/Gallery'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="galeria" element={<GalleryPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
